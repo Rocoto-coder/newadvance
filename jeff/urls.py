63ad6410e7e11c902ea487c00d39cbae5d08666b
Rocoto-coder <baravenas@usm.cl>
2025-02-13 12:55:31 -0300

    jejej

--- jeff/urls.py
+++ jeff/urls.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .views import inicio_view, bienvenida_view, contenido_view
 
+
 def inicio_view(request):
     template = loader.get_template('inicio.html')
     return HttpResponse(template.render({}, request))
